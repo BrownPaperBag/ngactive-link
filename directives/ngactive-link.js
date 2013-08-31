a342e411acd256f42cfa7bae165d20c4b857a7c9
Michael Robinson <mike@pagesofinterest.net>
2013-08-31 23:39:13 +1200

    Changed the code so it actually works.

--- directives/ngactive-link.js
+++ directives/ngactive-link.js
@@ -1,21 +1,42 @@
 angular.module('active-link', []).
   directive('active', ['$location',
     function(location) {
+
+      /**
+       * Add or remove the active class.
+       *
+       * @param {Object} element HTML element
+       * @param {Object} attr The element's attributes
+       */
+      function applyState(element, attr) {
+        var activeLinkClass = attr.active || 'active';
+        if (attr.href === location.path()) {
+          element.addClass(activeLinkClass);
+        } else {
+          element.removeClass(activeLinkClass);
+        }
+      }
+
       return {
         restrict: 'A',
-        link: function(scope, element, attrs, controller) {
-          var activeLinkClass = attrs.activeLink || 'active';
-          var path = attrs.href;
-          console.log(path);
-          path = path.substring(1);
-          scope.location = location;
-          scope.$watch('location.path()', function(newPath) {
-            if (path === newPath) {
-              element.addClass(activeLinkClass);
-            } else {
-              element.removeClass(activeLinkClass);
-            }
-          });
+
+        /**
+         * Apply the correct state to the element and return the directive's
+         * link function.
+         *
+         * @param {Object} element HTML element
+         * @param {Object} attr The element's attributes
+         * @return {Function} The directive's link function
+         */
+        compile: function(element, attr) {
+          applyState(element, attr);
+
+          return function(scope, element, attr, controller) {
+            scope.location = location;
+            scope.$watch('location.path()', function() {
+              applyState(element, attr);
+            });
+          }
         }
       };
     }
