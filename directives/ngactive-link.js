ef0c0fef2f54dd532ac40d035f6d8430eca54fa6
Michael Robinson <mike@pagesofinterest.net>
2013-08-31 22:38:51 +1200

    Added directive

---
+++ directives/ngactive-link.js
@@ -0,0 +1,22 @@
+ngular.module('active-link', []).
+  directive('active', ['$location',
+    function(location) {
+      return {
+        restrict: 'A',
+        link: function(scope, element, attrs, controller) {
+          var activeLinkClass = attrs.activeLink || 'active';
+          var path = attrs.href;
+          console.log(path);
+          path = path.substring(1);
+          scope.location = location;
+          scope.$watch('location.path()', function(newPath) {
+            if (path === newPath) {
+              element.addClass(activeLinkClass);
+            } else {
+              element.removeClass(activeLinkClass);
+            }
+          });
+        }
+      };
+    }
+  ]);
